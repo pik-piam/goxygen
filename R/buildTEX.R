66c8ee6afb7d87854b7d70cae02d608ac2051e93
giannou <giannou@pik-potsdam.de>
2019-06-14 16:31:12 +0200

    fixed pdflatex error reporting

--- R/buildTEX.R
+++ R/buildTEX.R
@@ -41,7 +41,8 @@ buildTEX <- function(file="documentation.tex", mdfolder="markdown", literature="
   message("...finished TEX creation!")
   if(pdf) {
     message("Start PDF creation...")
-    for(i in 1:2) tmp <- system(paste("pdflatex",file),intern=TRUE)
-    message("...finished PDF creation!")
+    for(i in 1:2) tmp <- system(paste("pdflatex -halt-on-error",file),intern=TRUE)
+    if(grepl("error",tail(tmp,2)[1])) writeLines(tmp,con ="goxygen_pdflatex.log")
+    message("...finished running pdflatex, check doc/goxygen_pdflatex.log if no PDF was created!")
   }
 }
