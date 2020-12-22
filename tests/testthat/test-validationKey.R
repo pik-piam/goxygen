12bda3538a43e814f33e2186c0cde1c8b639ef10
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2020-12-22 17:43:47 +0100

    updated tests

--- tests/testthat/test-validationKey.R
+++
@@ -1,7 +0,0 @@
-library(testthat)
-context("validation key")
-
-test_that("buildLibrary validation key is valid", {
-  folder <- ifelse(file.exists("DESCRIPTION"),".","../../")
-  expect_true(lucode2:::validkey(folder)$valid)
-})
