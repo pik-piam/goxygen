ff349bca0cb8d072dd04428425f3aa8747194818
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-09-21 15:44:23 +0200

    pass DoNotPlot in test

--- tests/testthat/test-goxygen.R
+++ tests/testthat/test-goxygen.R
@@ -3,7 +3,7 @@ skip_if_not(check_pandoc())
 test_that("extract documentation from modular dummy model", {
   docfolder <- file.path(withr::local_tempdir(), "doc_modular")
   out <- try(goxygen(path = system.file("dummymodel", package = "gms"), output = c("html", "tex"),
-                     docfolder = docfolder, includeCore = TRUE, cff = "HOWTOCITE.cff"))
+                     docfolder = docfolder, includeCore = TRUE, cff = "HOWTOCITE.cff", DoNotPlot = TRUE))
   expect_null(out)
   expect_true(file.exists(file.path(docfolder, "html", "index.htm")))
   expect_true(file.exists(file.path(docfolder, "html", "core.htm")))
@@ -16,7 +16,7 @@ test_that("extract HTML documentation from modular dummy model with classic styl
   docfolder <- file.path(withr::local_tempdir(), "doc_modular_classic")
   out <- try(goxygen(path = system.file("dummymodel", package = "gms"),
                      htmlStyle = "classic", output = "html",
-                     docfolder = docfolder, includeCore = TRUE, cff = "HOWTOCITE.cff"))
+                     docfolder = docfolder, includeCore = TRUE, cff = "HOWTOCITE.cff", DoNotPlot = TRUE))
   expect_null(out)
   expect_true(file.exists(file.path(docfolder, "html", "index.htm")))
   expect_true(file.exists(file.path(docfolder, "html", "core.htm")))
@@ -28,7 +28,7 @@ test_that("cache and unknown output", {
   docfolder <- file.path(withr::local_tempdir(), "doc_modular")
   expect_warning({
     out <- try(goxygen(path = system.file("dummymodel", package = "gms"), docfolder = docfolder,
-                       includeCore = TRUE, cache = TRUE, output = "bla"))
+                       includeCore = TRUE, cache = TRUE, output = "bla", DoNotPlot = TRUE))
   })
   expect_null(out)
 })
@@ -37,7 +37,7 @@ test_that("extract documentation from simple dummy model", {
   docfolder <- file.path(withr::local_tempdir(), "doc_simple")
   out <- try(goxygen(path = system.file("dummymodel", package = "gms"),
                      docfolder = docfolder, modularCode = FALSE,
-                     output = c("html", "tex"), cff = "HOWTOCITE.cff"))
+                     output = c("html", "tex"), cff = "HOWTOCITE.cff", DoNotPlot = TRUE))
   expect_null(out)
   expect_true(file.exists(file.path(docfolder, "html", "index.htm")))
   expect_true(file.exists(file.path(docfolder, "html", "modules_01_fancymodule_default_calculations.htm")))
