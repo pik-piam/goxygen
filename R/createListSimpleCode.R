a89a5fcb112f246d74be42d7175a3d93309cacd1
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2022-07-21 14:32:13 +0200

    - make sure that documentation from all gms files in a module gms realization are being extracted. Files which are not mentioned in realization.gms will be added at the end. - fixed various lintr warnings

--- R/createListSimpleCode.R
+++ R/createListSimpleCode.R
@@ -1,34 +1,33 @@
 #' createListSimpleCode
-#' 
+#'
 #' support function to create documentation of non-modular GAMS code.
-#' 
+#'
 #' @param path path to the model to be documented
 #' @param citation citation data read from a CFF file
 #' @param mainfile main file of the model
+#' @importFrom withr local_dir
 #' @author Jan Philipp Dietrich
 #' @seealso \code{\link{codeCheck}}
 
-createListSimpleCode <- function(path=".", citation=NULL, mainfile="main.gms") {
-  cwd <- getwd()
-  on.exit(setwd(cwd))
-  setwd(path)
-  
+createListSimpleCode <- function(path = ".", citation = NULL, mainfile = "main.gms") {
+
+  local_dir(path)
+
   # write doc files
   full <- list()
   data <- extractDocumentation(mainfile)
   data$citation <- citation
   full[["index"]] <- createIndexPage(data)
-  
-  files <- setdiff(list.files(pattern="\\.gms$",recursive = TRUE),mainfile)
-  for(f in files) {
-    fname <- sub("\\.gms$","",gsub("/","_",f,fixed = TRUE))
+
+  files <- setdiff(list.files(pattern = "\\.gms$", recursive = TRUE), mainfile)
+  for (f in files) {
+    fname <- sub("\\.gms$", "", gsub("/", "_", f, fixed = TRUE))
     data <- extractDocumentation(f)
-    if(length(data)>0) {
-      if(is.null(data$title)) data$title <- fname
+    if (length(data) > 0) {
+      if (is.null(data$title)) data$title <- fname
       data$name <- f
       full[[fname]] <- createSimplePage(data)
     }
   }
   return(full)
 }
-
