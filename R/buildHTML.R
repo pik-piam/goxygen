9a3723e2c6696ce2177d559a4321b5c8912a7221
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2022-09-30 15:45:13 +0200

    - tweaked ming template - allow templates to add more files to the html folder

--- R/buildHTML.R
+++ R/buildHTML.R
@@ -21,97 +21,98 @@
 #' @author Jan Philipp Dietrich
 #' @seealso \code{\link{goxygen}}, \code{\link{buildTEX}}
 #' @importFrom utils packageVersion
+#' @importFrom withr local_dir
 #' @export
 
-buildHTML <- function(style="classic", folder="html", mdfolder="markdown", literature="literature.bib", citation="../CITATION.cff", supplementary="images", debug=FALSE, templatefolder="..") {
+buildHTML <- function(style = "classic", folder = "html", mdfolder = "markdown", literature = "literature.bib",
+                      citation = "../CITATION.cff", supplementary = "images", debug = FALSE, templatefolder = "..") {
 
   # check style
-  if(style=="classic") return(oldBuildHTML(folder=folder, mdfolder=mdfolder, literature=literature, citation=citation, supplementary=supplementary))
+  if (style == "classic") return(oldBuildHTML(folder = folder, mdfolder = mdfolder, literature = literature,
+                                              citation = citation, supplementary = supplementary))
 
-  templatefileCSS   <- chooseTemplate(style,templatefolder,"css")
-  templatefileHTML5 <- chooseTemplate(style,templatefolder,"html5")
+  templateFolder   <- chooseTemplate(style, templatefolder)
+  templatefileHTML5 <- chooseTemplate(style, templatefolder, "html5")
 
   message("Start HTML creation...")
-  check_pandoc(error=TRUE)
+  check_pandoc(error = TRUE)
 
   # check available md files
-  files <- list.files(mdfolder,pattern=".*\\.md$",full.names = TRUE)
-  moduleNames <- sub("\\.[^.]*$","",basename(files))
+  files <- list.files(mdfolder, pattern = ".*\\.md$", full.names = TRUE)
+  moduleNames <- sub("\\.[^.]*$", "", basename(files))
 
   # prepare folder
-  if(!dir.exists(folder)) dir.create(folder)
-  file.copy(templatefileCSS,paste0(folder,"/style.css"))
-  for(elem in supplementary) file.copy(elem,folder,recursive = TRUE, overwrite = TRUE)
+  if (!dir.exists(folder)) dir.create(folder)
+  file.copy(templateFolder, folder, recursive = TRUE)
+  for (elem in supplementary) file.copy(elem, folder, recursive = TRUE, overwrite = TRUE)
 
   # prepare reference file
-  ref <- ifelse(debug, "ref.md", tempfile())
-  returnReferences(moduleNames,paste0(moduleNames,".htm"),ref, level=2)
+  ref <- ifelse(debug, "ref.md", tempfile()) # nolint
+  returnReferences(moduleNames, paste0(moduleNames, ".htm"), ref, level = 2)
 
   # prepare main navigation
   returnHTMLref <- function(files) {
-    out <- data.frame(name=NULL, title=NULL, stringsAsFactors = FALSE)
-    for(f in files) {
-      tmp <- readLines(f,n=2)
+    out <- data.frame(name = NULL, title = NULL, stringsAsFactors = FALSE)
+    for (f in files) {
+      tmp <- readLines(f, n = 2)
       # use title if title is detected
       out <- rbind(out,
-                   data.frame(title = ifelse(grepl("^=*$",tmp[2]), sub(" *\\(.*$","",tmp[1]), ""),
-                              name = sub(".md","",basename(f),fixed=TRUE), stringsAsFactors = FALSE))
+                   data.frame(title = ifelse(grepl("^=*$", tmp[2]), sub(" *\\(.*$", "", tmp[1]), ""),
+                              name = sub(".md", "", basename(f), fixed = TRUE), stringsAsFactors = FALSE))
     }
     return(out)
   }
-  mainNav <- function(mdfolder){
-    cwd <- getwd()
-    setwd(mdfolder)
-    on.exit(setwd(cwd))
+  mainNav <- function(mdfolder) {
+    local_dir(mdfolder)
     ref <- returnHTMLref(dir(".", pattern = "\\.md$"))
-    ref$title[ref$title==""] <- ref$name[ref$title==""]
+    ref$title[ref$title == ""] <- ref$name[ref$title == ""]
 
-    bringToFront <- function(ref,name,newname=NULL) {
-      if(name%in%ref$name) {
-        #bring index page to the front
-        i <- which(ref$name==name)
-        ref <- ref[c(i,setdiff(1:nrow(ref),i)),]
-        #rename index to overview
-        if(!is.null(newname)) ref$title[1] <- newname
+    bringToFront <- function(ref, name, newname = NULL) {
+      if (name %in% ref$name) {
+        # bring index page to the front
+        i <- which(ref$name == name)
+        ref <- ref[c(i, setdiff(seq_len(nrow(ref)), i)), ]
+        # rename index to overview
+        if (!is.null(newname)) ref$title[1] <- newname
       }
       return(ref)
     }
-    ref <- bringToFront(ref,"core")
-    ref <- bringToFront(ref,"index","Overview")
+    ref <- bringToFront(ref, "core")
+    ref <- bringToFront(ref, "index", "Overview")
 
-    has_number <- grepl("^[0-9]{1,2}_",ref$name)
-    if(sum(has_number)>=(nrow(ref)-2)) {
+    hasNumber <- grepl("^[0-9]{1,2}_", ref$name)
+    if (sum(hasNumber) >= (nrow(ref) - 2)) {
       # if all names (expect of one, which might be the index page) begin
       # with a number, use that number in front of the title
-      number <- as.integer(sub("_.*$","",ref$name[has_number]))
-      ref$title[has_number] <- paste0(format(number,width=2),". ",ref$title[has_number])
+      number <- as.integer(sub("_.*$", "", ref$name[hasNumber]))
+      ref$title[hasNumber] <- paste0(format(number, width = 2), ". ", ref$title[hasNumber])
     }
     return(ref)
   }
   mainNav <- mainNav(mdfolder)
 
-  mainpage <- ifelse("index" %in% mainNav$name," -V mainpage=\"index.htm\"","")
+  mainpage <- ifelse("index" %in% mainNav$name, " -V mainpage=\"index.htm\"", "")
 
-  if(is.character(citation) && file.exists(citation)) {
+  if (is.character(citation) && file.exists(citation)) {
     citation <- read_cff(citation)
-  } else if(!is.list(citation)) {
-    citation <- list(title="Model Documentation")
+  } else if (!is.list(citation)) {
+    citation <- list(title = "Model Documentation")
   }
-  repo <- ifelse(!is.null(citation$`repository-code`),paste0(" -V repo=",citation$`repository-code`),"")
-  bib <- ifelse(file.exists(literature),paste0("--bibliography=",literature),"")
-  logo <- ifelse(file.exists(paste0(folder,"/images/logo.png")), " -V logo", "")
+  repo <- ifelse(!is.null(citation$`repository-code`), paste0(" -V repo=", citation$`repository-code`), "")
+  bib <- ifelse(file.exists(literature), paste0("--bibliography=", literature), "")
+  logo <- ifelse(file.exists(file.path(folder, "images/logo.png")), " -V logo", "")
   authors <- ""
-  if(!is.null(citation$authors)) {
-    .tmp <- function(x) return(paste(x$`given-names`,x$`family-names`))
-    authors <- paste0(" -V author-meta=\"", sapply(citation$authors,.tmp),"\"")
-    authors <- paste(rev(authors),collapse="")
+  if (!is.null(citation$authors)) {
+    .tmp <- function(x) return(paste(x$`given-names`, x$`family-names`))
+    authors <- paste0(" -V author-meta=\"", vapply(citation$authors, .tmp, character(1)), "\"")
+    authors <- paste(rev(authors), collapse = "")
   }
 
-  for(m in moduleNames) {
-    pagetitle <- mainNav$title[mainNav$name==m]
-    if(length(pagetitle)==0) pagetitle <- m
-    ofile <- paste0(folder,"/",m,".htm")
-    pandoc_call <-paste0("pandoc ",mdfolder,"/",m,".md ",ref," -o ",ofile,
+  for (m in moduleNames) {
+    pagetitle <- mainNav$title[mainNav$name == m]
+    if (length(pagetitle) == 0) pagetitle <- m
+    ofile <- paste0(folder, "/", m, ".htm")
+    pandocCall <- paste0("pandoc ", mdfolder, "/", m, ".md ", ref, " -o ", ofile,
                          " --css template.css ",
                          bib,
                          authors,
@@ -119,16 +120,17 @@ buildHTML <- function(style="classic", folder="html", mdfolder="markdown", liter
                          repo,
                          mainpage,
                          " --toc --mathjax --standalone --metadata link-citations=true",
-                         " --template=",templatefileHTML5,
-                         " --metadata title=",m,
-                         " -V modeltitle=\"",citation$title,"\"",
-                         " -V goxygenversion=",packageVersion("goxygen"),
-                         " -V modelversion=",citation$version,
-                         " -V pagetitle=\"",pagetitle,"\"",
-                         " -V mainnav=\"",paste0("<a href='",mainNav$name,".htm'>",mainNav$title,"</a>",collapse="\n"),"\"")
-    if(debug) message(pandoc_call,"\n")
-    system(pandoc_call)
+                         " --template=", templatefileHTML5,
+                         " --metadata title=", m,
+                         " -V modeltitle=\"", citation$title, "\"",
+                         " -V goxygenversion=", packageVersion("goxygen"),
+                         " -V modelversion=", citation$version,
+                         " -V pagetitle=\"", pagetitle, "\"",
+                         " -V mainnav=\"", paste0("<a href='", mainNav$name, ".htm'>", mainNav$title, "</a>",
+                                                  collapse = "\n"), "\"")
+    if (debug) message(pandocCall, "\n") # nolint
+    system(pandocCall)
   }
-  if(!debug) unlink(ref)
+  if (!debug) unlink(ref) # nolint
   message("...finished HTML creation!")
 }
--- R/goxygen.R
+++ R/goxygen.R
@@ -1,16 +1,16 @@
 #' goxygen
-#' 
-#' Documentation function which extracts a full model documentation from a 
+#'
+#' Documentation function which extracts a full model documentation from a
 #' modularized gams model. The function extracts comments used as documentation,
 #' extracts code and can extract and convert GAMS equations as latex code. Output
 #' is returned in Markdown, HTML and PDF format.
-#' 
+#'
 #' @note Documentation lines in the code must start with *' to be detected as documentation.
-#' Identifier at the beginning of each block describe what kind of documentation is given. 
+#' Identifier at the beginning of each block describe what kind of documentation is given.
 #' All identifiers start with @ followed by the name of the identifier. Currently, following
 #' identifiers are available
 #' \itemize{
-#'  \item @title Title 
+#'  \item @title Title
 #'  \item @authors List of authors
 #'  \item @description Model description (only the documentation text will be interpreted)
 #'  \item @equations Equation description (documentation text will be extracted and gams equations
@@ -20,27 +20,28 @@
 #'  \item @stop everything following will be ignored until the next identifier is mentioned again. Useful
 #'  to stop a section
 #' }
-#' 
+#'
 #' In addition you can store a model logo (100px height, 100px weight) as \code{logo.png} in the main
 #' folder of the model which then will be used in the HTML version of the documentation.
-#' If you want to add citations to your documentation you can do so by adding a bibtex file with 
+#' If you want to add citations to your documentation you can do so by adding a bibtex file with
 #' the name literature.bib in the main folder of the model. To link these references in the text
-#' you can use the syntax \code{@<id>} in which "<id>" stands for the identifier given to the 
+#' you can use the syntax \code{@<id>} in which "<id>" stands for the identifier given to the
 #' corresponding bibtex entry.
-#' 
+#'
 #' @param path path to the model to be documented
 #' @param docfolder folder the documentation should be written to relative to model folder
 #' @param cache Boolean to allow read data from existing cache file
-#' @param htmlStyle visualization style to be used for the HTML creation. Currently available styles are 
+#' @param htmlStyle visualization style to be used for the HTML creation. Currently available styles are
 #' "classic" and "ming". Ignored for outputs other than HTML. Can be extended by additional templates stored in the
-#' \code{templatefolder} in the format \code{<style>.css} and \code{<style>.html5} (both need to be provided). 
-#' The preinstalled ming template \code{system.file("templates","ming.css",package="goxygen")}  and 
-#' \code{system.file("templates","ming.html5",package="goxygen")} can serve as a starting point for own 
+#' \code{templatefolder} in the form \code{<style>.html5} together with a subfolder with supplementary files
+#' and the name of the style \code{<style>} (both need to be provided).
+#' The preinstalled ming template \code{system.file("templates","ming.css",package="goxygen")}  and
+#' \code{system.file("templates","ming.html5",package="goxygen")} can serve as a starting point for own
 #' templates.
-#' @param texStyle visualization style to be used for the Latex/PDF creation. Currently only "classic" style is 
+#' @param texStyle visualization style to be used for the Latex/PDF creation. Currently only "classic" style is
 #' available. Ignored for outputs other than Latex/PDF. Can be extended by additional templates stored in the
-#' \code{templatefolder} in the format \code{<style>.latex}. Classic template 
-#' \code{system.file("templates","classic.latex",package="goxygen")} can serve as a starting point for own 
+#' \code{templatefolder} in the format \code{<style>.latex}. Classic template
+#' \code{system.file("templates","classic.latex",package="goxygen")} can serve as a starting point for own
 #' templates.
 #' @param templatefolder Folder in which goxygen will search for template files in addition to the pre-installed ones.
 #' @param output List of output to be written, available are "html","pdf" and "tex"
@@ -49,7 +50,8 @@
 #' @param unitPattern pattern that is usedto identify the unit in the description, default =c("\\(","\\)")
 #' @param includeCore boolean whether core should be included or not, default=FALSE
 #' @param mainfile main file of the model
-#' @param ... optional arguments to \code{\link[gms]{interfaceplot}}, passed via \code{\link[gms]{modules_interfaceplot}}. 
+#' @param ... optional arguments to \code{\link[gms]{interfaceplot}}, passed via
+#' \code{\link[gms]{modules_interfaceplot}}.
 #'
 #' @author Jan Philipp Dietrich
 #' @importFrom stringi stri_extract_all_regex stri_replace_all_regex stri_write_lines
@@ -58,83 +60,85 @@
 #' @importFrom citation read_cff cff2bibentry
 #' @importFrom yaml as.yaml
 #' @importFrom utils tail toBibtex capture.output
+#' @importFrom withr local_dir
 #' @seealso \code{\link{codeCheck}},\code{\link[gms]{interfaceplot}}
 #' @examples
 #' # make sure that pandoc is available
-#' if(check_pandoc()) {
+#' if (check_pandoc()) {
 #'   # run goxygen for dummy model and store documentation as HTML in a temporary directory
-#'   docfolder <- paste0(tempdir(),"/doc")
-#'   goxygen(system.file("dummymodel",package="gms"),  docfolder=docfolder, output="html")
+#'   docfolder <- paste0(tempdir(), "/doc")
+#'   goxygen(system.file("dummymodel", package = "gms"),  docfolder = docfolder, output = "html")
 #' }
 #' @export
-goxygen <- function(path=".", 
-                    docfolder="doc", 
-                    cache=FALSE, 
-                    output=c("html","tex","pdf"), 
-                    htmlStyle="ming",
-                    texStyle="classic",
-                    templatefolder=".",
-                    cff="CITATION.cff", 
-                    modularCode=is.modularGAMS(), 
-                    unitPattern=c("\\(","\\)"), 
-                    includeCore=FALSE, 
-                    mainfile="main.gms",
+goxygen <- function(path = ".",
+                    docfolder = "doc",
+                    cache = FALSE,
+                    output = c("html", "tex", "pdf"),
+                    htmlStyle = "ming",
+                    texStyle = "classic",
+                    templatefolder = ".",
+                    cff = "CITATION.cff",
+                    modularCode = is.modularGAMS(),
+                    unitPattern = c("\\(", "\\)"),
+                    includeCore = FALSE,
+                    mainfile = "main.gms",
                     ...) {
-  cwd <- getwd()
-  on.exit(setwd(cwd))
-  setwd(path)
+  local_dir(path)
 
   templatefolder <- normalizePath(templatefolder)
-  
-  if(file.exists(cff)) {
+
+  if (file.exists(cff)) {
     citation <- read_cff(cff)
   } else {
     citation <- NULL
   }
-  
-  if(!dir.exists(docfolder)) dir.create(docfolder, recursive = TRUE)
-  if(file.exists("literature.bib")) file.copy("literature.bib",paste0(docfolder,"/literature.bib"), overwrite = TRUE)
 
-  copyimages <- function(docfolder,paths) {
-    imagefolder <- paste0(docfolder,"/images")
-    if(!dir.exists(imagefolder)) dir.create(imagefolder, recursive=TRUE)
-    file.copy(Sys.glob(paths),imagefolder,overwrite = TRUE)
+  if (!dir.exists(docfolder)) dir.create(docfolder, recursive = TRUE)
+  if (file.exists("literature.bib")) file.copy("literature.bib", paste0(docfolder, "/literature.bib"), overwrite = TRUE)
+
+  copyimages <- function(docfolder, paths) {
+    imagefolder <- paste0(docfolder, "/images")
+    if (!dir.exists(imagefolder)) dir.create(imagefolder, recursive = TRUE)
+    file.copy(Sys.glob(paths), imagefolder, overwrite = TRUE)
   }
-    
-  if(modularCode) {
+
+  if (modularCode) {
     copyimages(docfolder, paths = c("*.png",
                                     "*.jpg",
                                     "modules/*/*.png",
                                     "modules/*/*/*.png",
                                     "modules/*/*.jpg",
                                     "modules/*/*/*.jpg"))
-    cachefile <- paste0(docfolder,"/doc.rds")
-    if(cache & file.exists(cachefile)) {
+    cachefile <- paste0(docfolder, "/doc.rds")
+    if (cache && file.exists(cachefile)) {
       cache <- readRDS(cachefile)
       cc <- cache$cc
       interfaces <- cache$interfaces
     } else {
-      cc <- codeCheck(details=TRUE)
-      interfaces <- modules_interfaceplot(cc$interfaceInfo, 
-                                          targetfolder= paste0(docfolder,"/images"), 
-                                          writetable=FALSE,
-                                          includeCore=includeCore,
+      cc <- codeCheck(details = TRUE)
+      interfaces <- modules_interfaceplot(cc$interfaceInfo,
+                                          targetfolder = paste0(docfolder, "/images"),
+                                          writetable = FALSE,
+                                          includeCore = includeCore,
                                           ...)
-      saveRDS(list(cc=cc,interfaces=interfaces),cachefile)
-    }  
-    full <- createListModularCode(cc=cc, interfaces=interfaces, path=".", citation=citation, unitPattern=unitPattern, includeCore=includeCore, mainfile=mainfile, docfolder=docfolder)
+      saveRDS(list(cc = cc, interfaces = interfaces), cachefile)
+    }
+    full <- createListModularCode(cc = cc, interfaces = interfaces, path = ".", citation = citation,
+                                  unitPattern = unitPattern, includeCore = includeCore,
+                                  mainfile = mainfile, docfolder = docfolder)
   } else {
-    copyimages(docfolder, paths=list.files(pattern="\\.(jpg|png)$",recursive = TRUE))
-    full <- createListSimpleCode(path=".", citation=citation, mainfile=mainfile)
+    copyimages(docfolder, paths = list.files(pattern = "\\.(jpg|png)$", recursive = TRUE))
+    full <- createListSimpleCode(path = ".", citation = citation, mainfile = mainfile)
   }
-  
-  setwd(docfolder)
 
-  if(any(nomatch <- !(output %in% c("html","pdf","tex")))){
-    warning(paste0("No output format '",output[nomatch],"' available. It will be ignored."))
+  local_dir(docfolder)
+
+  if (any(nomatch <- !(output %in% c("html", "pdf", "tex")))) {
+    warning(paste0("No output format '", output[nomatch], "' available. It will be ignored."))
   }
   buildMarkdown(full)
-  if("html"%in% output) buildHTML(supplementary="images", citation=citation, style=htmlStyle, templatefolder=templatefolder)
-  if("tex" %in% output | "pdf" %in% output) buildTEX(pdf=("pdf" %in% output), citation=citation, style=texStyle, templatefolder=templatefolder)
+  if ("html" %in% output) buildHTML(supplementary = "images", citation = citation, style = htmlStyle,
+                                    templatefolder = templatefolder)
+  if ("tex" %in% output || "pdf" %in% output) buildTEX(pdf = ("pdf" %in% output), citation = citation, style = texStyle,
+                                                      templatefolder = templatefolder)
 }
-  
